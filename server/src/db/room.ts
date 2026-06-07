2b894fd013ae79e022a43bce41cc15874ceba9f9
mohitagarwal11 <agarwalmohit1011@gmail.com>
2026-06-07 20:55:33 +0530

    New endpoints -getPublicRooms -getRoomById

--- server/src/db/room.ts
+++ server/src/db/room.ts
@@ -1,7 +1,7 @@
 import sql from './client';
 import generateRoomCode from '../utils/generateRoomCode';
 
-export default async function createRoom(data: {
+export async function createRoom(data: {
   name: string;
   isPrivate: boolean;
   maxPlayers: number;
@@ -29,6 +29,38 @@ export default async function createRoom(data: {
     )
     RETURNING *
   `;
+
+  return room;
+}
+
+export async function getPublicRooms() {
+  return await sql`
+    SELECT
+      id,
+      code,
+      name,
+      max_players,
+      total_rounds,
+      created_at
+    FROM rooms
+    WHERE is_private = false
+      AND status = 'waiting'
+    ORDER BY created_at DESC
+  `;
+}
+
+export async function getRoomById(roomId: string) {
+  console.log('SEARCHING FOR:', roomId);
+
+  const result = await sql`
+    SELECT *
+    FROM rooms
+    WHERE id = ${roomId}
+  `;
+
+  const [room] = result;
+
+  console.log('ROOM:', room);
 
   return room;
 }
--- server/src/routes/routes.rooms.ts
+++ server/src/routes/routes.rooms.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import createRoom from '../db/room';
+import { createRoom, getPublicRooms, getRoomById } from '../db/room';
 
 const router = Router();
 
-router.post('/create', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const room = await createRoom(req.body);
     res.status(201).json(room);
@@ -11,6 +11,38 @@ router.post('/create', async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: 'Failed to create room.',
+    });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const rooms = await getPublicRooms();
+    res.status(200).json(rooms);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to get public rooms.',
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const room = await getRoomById(req.params.id);
+
+    if (!room) {
+      return res.status(404).json({
+        message: 'Room not found',
+      });
+    }
+
+    res.status(200).json(room);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: 'Failed to get room by id.',
     });
   }
 });
